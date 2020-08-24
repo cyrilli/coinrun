260f1941c63fd454808c900333d68d5c17d7a094
CLi <chuanhao1994@gmail.com>
2020-08-24 18:10:10 -0400

    tune hyper params

--- coinrun/config_dqn.py
+++ coinrun/config_dqn.py
@@ -55,8 +55,8 @@ class ConfigSingle_(object):
         type_keys.append(('arch', 'architecture', str, 'cnn', True))
         type_keys.append(('lr', 'lr', float, 1e-4))
         type_keys.append(('batch_size', 'batch_size', int, 64))
-        type_keys.append(('total_timesteps', 'total_timesteps', int, 2000000))
-        type_keys.append(('buffer_size', 'buffer_size', int, 10000))
+        type_keys.append(('total_timesteps', 'total_timesteps', int, 10000000))
+        type_keys.append(('buffer_size', 'buffer_size', int, 1000000))
 
         # Overwrite the latest save file after this many updates
         type_keys.append(('ckpt_freq', 'checkpoint_freq', int, 10))
